c58473d666f286199403e0653869b7b08514b90c
a_nedaseikin <andrei.nedaseikin@gmail.com>
2021-05-13 21:41:18 +0300

    feat: create mock implementation of getAll method for users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,32 @@
-const getAll = async () => {
+const getAll = async () => 
   // TODO: mock implementation. should be replaced during task development
-  return [];
-};
+   [
+     {
+       id: 'user1',
+       name: 'First User',
+       login: 'First',
+     },
+     {
+       id: 'user2',
+       name: 'Second User',
+       login: 'Second',
+     },
+     {
+       id: 'user3',
+       name: 'Third User',
+       login: 'Third',
+     },
+     {
+       id: 'user4',
+       name: 'Fourth User',
+       login: 'Fourth',
+     },
+     {
+       id: 'user5',
+       name: 'Fifth User',
+       login: 'Fifth',
+     },
+   ]
+;
 
 module.exports = { getAll };
