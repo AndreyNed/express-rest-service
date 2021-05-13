a8a960ff2659f2c9ec53a41f4050f0fcc01ef90a
a_nedaseikin <andrei.nedaseikin@gmail.com>
2021-05-13 21:30:10 +0300

    feat: create placeholders for users routes

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -8,4 +8,24 @@ router.route('/').get(async (req, res) => {
   res.json(users.map(User.toResponse));
 });
 
+router.route('/').post(async (req, res) => {
+  // Creates a new user (remove password from response)
+  res.sendStatus(501);
+});
+
+router.route('/:userId').get(async (req, res) => {
+  // Gets a user by ID e.g. “/users/123” (remove password from response)
+  res.sendStatus(501);
+});
+
+router.route('/:userId').put(async (req, res) => {
+  // Updates a user by ID
+  res.sendStatus(501);
+});
+
+router.route('/:userId').delete(async (req, res) => {
+  // Delete a user by ID
+  res.sendStatus(501);
+});
+
 module.exports = router;
